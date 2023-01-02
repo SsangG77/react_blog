7dc04ee3de36d138c98e4e1df3f51abbf9900ff4
[sangjin] <sangjincha719@gmail.com>
2023-01-02 22:42:24 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+let title = 'aaaaaaaaaaaaa'
+let styleObj = {
+  color : 'red',
+  fontSize : '40px'
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black_nav'>
+        <h4>블로그</h4>
+      </div>
+      <h3 style= {styleObj}>{title}</h3>
     </div>
   );
 }
