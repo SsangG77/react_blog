79ce6b64eaa37f1bc30a7d2b79d3762bdc5d0bc9
[sangjin] <sangjincha719@gmail.com>
2023-01-05 22:34:55 +0900

    learn_props

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import {useState} from "react"
 
 function App() {
   
-
-
   let [title, setTitle] = useState(['노션 그래프화 하기', '갤럭시 폴드 5', '좌식의자 구매후기']);
   let [num, numCount] = useState([0,0,0]); 
-  let [num1, numCount1] = useState(0)
-  let [num2, numCount2] = useState(0)
+  
+  let [modal, setModal] = useState(true);
 
 
-  let [total, totalCount] = useState([0,0,0])
+  function changeTitle(i) {
+    let copy = [...title];
+    copy[i] = '여자 코트 추천'
+    setTitle(copy)
+
+  }
 
   return (
     <div className="App">
@@ -29,51 +32,18 @@ function App() {
     }}>
       정렬
     </button>
-
-      {/* <h3 style= {styleObj}>{titlet}</h3>
-      <Write/>
-      <div className='list'>
-        <h4>
-          { title[1] }
-          <span onClick={() => setCoat('여자 코트 추천')}> Click!</span> { 0 }
-          </h4>
-        <p>22/02/17</p>
-      </div>
-      <div className='list'>
-        <h4>{ title[2] }</h4>
-        <p>22/02/17</p>
-      </div>
-
-      <Modal/> */}
-
-      {
-    //     <>
-    //     <div className='list'>
-    //       <h4>
-    //         {title[0]}
-    //         <span onClick={() => {numCount(num + 1)}}>👍 {num}</span>
-    //       </h4>
-    //     </div>
-    //     <div className='list'>
-    //     <h4>
-    //       {title[1]}
-    //       <span onClick={() => {numCount1(num1 + 1)}}>👍 {num1}</span>
-    //     </h4>
-    //   </div>
-    //   <div className='list'>
-    //   <h4>
-    //     {title[2]}
-    //     <span onClick={() => {numCount2(num2 + 1)}}>👍 {num2}</span>
-    //   </h4>
-    // </div>
-    //     </>
-      }
       {
         title.map(function(a, i) {
           return (
 
             <div className='list' key={i}>
-            <h4>
+            <h4 onClick={()=> {
+              if(modal == false) {
+                setModal(true)
+              } else {
+                setModal(false)
+              }
+            }}>
               { a }
               <span onClick={() => {
                 let copy = [...num];
@@ -85,14 +55,25 @@ function App() {
           </div>
             )
         })
+        
       }
-      
+      {
+        modal ? <Modal 색="yellow" title={title} 제목변경={() => changeTitle(1)}></Modal> : <></>
+      }
     </div>
   );
 }
 
 
-
-
+function Modal(props) {
+  return (
+    <div className='modal' style={{background : props.색}}>
+      <h4>{props.title[0]}</h4>
+      <p>2022/12/25</p>
+      <p>상세 내용들</p>
+      <button onClick={props.제목변경}>글수정</button>
+    </div>
+  )
+}
 
 export default App;
