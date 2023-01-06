1b886f2a1e5fec23c6450573edeb4004e881f76f
[sangjin] <sangjincha719@gmail.com>
2023-01-07 00:33:04 +0900

    review

--- src/App.js
+++ src/App.js
@@ -6,18 +6,14 @@ import {useState} from "react"
 
 function App() {
   
-  let [title, setTitle] = useState(['노션 그래프화 하기', '갤럭시 폴드 5', '좌식의자 구매후기']);
+  let [titles, setTitles] = useState(['노션 그래프화 하기', '갤럭시 폴드 5', '좌식의자 구매후기']);
   let [num, numCount] = useState([0,0,0]); 
   
-  let [modal, setModal] = useState(true);
+  let [modal, setModal] = useState(false);
+  
+  let [title, setTitle] = useState("");
+  let [input, setInput] = useState("");
 
-
-  function changeTitle(i) {
-    let copy = [...title];
-    copy[i] = '여자 코트 추천'
-    setTitle(copy)
-
-  }
 
   return (
     <div className="App">
@@ -25,21 +21,15 @@ function App() {
         <h4>블로그</h4>
       </div>
 
-    <button onClick={() => {
-      let copy = [...title];
-      setTitle(copy.sort())
-      console.log(title)
-    }}>
-      정렬
-    </button>
       {
-        title.map(function(a, i) {
+        titles.map(function(a, i) {
           return (
 
             <div className='list' key={i}>
             <h4 onClick={()=> {
               if(modal == false) {
                 setModal(true)
+                setTitle(titles[i])
               } else {
                 setModal(false)
               }
@@ -51,14 +41,33 @@ function App() {
                   numCount(copy)
                 }}>👍</span> {num[i]}
             </h4>
+            <button onClick={()=>{
+              let copy = [...titles];
+              copy.splice(i, 1)
+              setTitles(copy);
+            }}>삭제</button>
             <p>2023/01/15</p>
           </div>
             )
         })
         
       }
+
+      <input type="text" onChange={(e)=> { 
+        setInput(e.target.value)
+        console.log(e.target.value)
+        }}></input>
+        <button onClick={() => {
+          let text = input;
+          let copy = [...titles];
+          
+          copy[copy.length] = text;
+          console.log(copy);
+          setTitles(copy);
+          // setTitles[copy];
+        }}>글추가</button>
       {
-        modal ? <Modal 색="yellow" title={title} 제목변경={() => changeTitle(1)}></Modal> : <></>
+        modal ? <Modal 색="green" title={title}></Modal> : <></>
       }
     </div>
   );
@@ -67,11 +76,11 @@ function App() {
 
 function Modal(props) {
   return (
-    <div className='modal' style={{background : props.색}}>
-      <h4>{props.title[0]}</h4>
+    <div className='modal'>
+      <h4>{props.title}</h4>
       <p>2022/12/25</p>
       <p>상세 내용들</p>
-      <button onClick={props.제목변경}>글수정</button>
+      <button>글수정</button>
     </div>
   )
 }
